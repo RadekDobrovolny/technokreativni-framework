978580cc353c0127ac63d9e863e0f6ad80defe14
Radek Dobrovolný <radek.dobrovolny@gmail.com>
2020-01-15 15:47:01 +0100

    Build webu

---
+++ docs/en.search.min.ee6c147e9f4d18de848aee2c35a5553a38a580e089264068b18b1b719bc76e0b.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/technokreativni-framework/flexsearch.min.js');loadScript('/technokreativni-framework/en.search-data.min.ac3111dc6460ddc1b56cb38ecc97c6cc99720823c4da4933ac25591bb3bb83ab.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
